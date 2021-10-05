b87a55f78040dcdc2e72b564962b37e32e1a7ee7
Clovis Nunes <clobnjuh@hotmail.com>
2021-10-05 13:47:00 -0300

    schedule

--- secretary.py
+++ secretary.py
@@ -1,7 +1,11 @@
 import os
+import schedule
+import time
 
 from playsound import playsound
 from gtts import gTTS
+
+# agendar com schedule a cada hora(s) e iniciar com o sitema
 
 def adicionar_tarefa(tarefa):
     global tasks
@@ -21,6 +25,8 @@ def ler_tarefas():
 
 # exibir menu inicial
 
+#schedule.every(2).hour.do(ler_tarefas)
+
 tasks = 'Finalizar campos do cadastro, Refazer requisição POST, Remodelar banco de dados e backend.' # transformar em array, salvar e ler de arquivo
 tasknum = 3
 intro = 'Olá Clóvis, você tem ' + str(tasknum) + ' tarefas a fazer.'
@@ -31,3 +37,7 @@ full_speech = gTTS(text=total_phrase, lang='pt', tld='com.br', slow=False)
 full_speech.save("fullspeech.mp3")
 
 playsound('fullspeech.mp3')
+
+""" while True:
+    schedule.run_pending()
+    time.sleep(1) """
